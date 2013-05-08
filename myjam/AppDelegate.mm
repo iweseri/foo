aaa1bb8951b26fdf2d7f2f11d769e332d7a4faff
apiztol <apiztol@gmail.com>
2013-05-08 13:02:57 +0800

    get device token and read notif

--- myjam/AppDelegate.mm
+++ myjam/AppDelegate.mm
@@ -81,6 +81,12 @@ NSString *const FBSessionStateChangedNotification = @"com.threezquare.jambu:FBSe
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+    
+    // Let the device know we want to receive push notifications
+	[[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    
+    
     // Create socket nodejs client
     socketIO = [[SocketIO alloc] initWithDelegate:self];
     [self initViews];
@@ -1193,9 +1199,44 @@ NSString *const FBSessionStateChangedNotification = @"com.threezquare.jambu:FBSe
 }
 
 #pragma mark -
+#pragma mark APNS delegate
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+	NSLog(@"My token is: %@", deviceToken);
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	NSLog(@"Failed to get token, error: %@", error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    UIApplicationState state = [application applicationState];
+    if (state == UIApplicationStateActive) {
+//        NSString *cancelTitle = @"Close";
+//        NSString *showTitle = @"Show";
+//        NSString *message = [[userInfo valueForKey:@"aps"] valueForKey:@"alert"];
+//        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Some title"
+//                                                            message:message
+//                                                           delegate:self
+//                                                  cancelButtonTitle:cancelTitle
+//                                                  otherButtonTitles:showTitle, nil];
+//        [alertView show];
+//        [alertView release];
+        NSLog(@"%@",userInfo);
+    } else {
+        //Do stuff that you would do if the application was not active
+        NSLog(@"not active: %@",userInfo);
+    }
+}
+
+#pragma mark -
+#pragma mark cycle delegate
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+    
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -1204,11 +1245,15 @@ NSString *const FBSessionStateChangedNotification = @"com.threezquare.jambu:FBSe
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//    [self disconnectNodeJS];
+//    NSLog(@"-disconnect");
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+//    [self connectNodeJS];
+//    NSLog(@"-connect");
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -1225,7 +1270,7 @@ NSString *const FBSessionStateChangedNotification = @"com.threezquare.jambu:FBSe
             [localData synchronize];
             [self initViews];
         }
-        
+
         [FBSettings publishInstall:kAppID];
         [FBSession.activeSession handleDidBecomeActive]; //fb login
     }
